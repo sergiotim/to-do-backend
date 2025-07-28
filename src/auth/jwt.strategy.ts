17458efb279daf02ba5dceeab65225151a08073c
Sérgio Timoteo <74915883+sergiotim@users.noreply.github.com>
2025-07-28 00:29:25 -0300

    excluindo coisas desnecessárias

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,7 +2,6 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { emitWarning } from 'process';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
